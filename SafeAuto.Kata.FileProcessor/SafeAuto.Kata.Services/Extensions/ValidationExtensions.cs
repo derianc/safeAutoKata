074461146a1c5eb42ab37d120e20feaeb34cfddc
derian <derian.cmorgan@priorauthnow.com>
2019-06-13 23:33:28 -0400

    consolidate Extension files

--- SafeAuto.Kata.FileProcessor/SafeAuto.Kata.Services/Extensions/ValidationExtensions.cs
+++ SafeAuto.Kata.FileProcessor/SafeAuto.Kata.Services/Extensions/ValidationExtensions.cs
@@ -23,5 +23,28 @@ namespace SafeAuto.Kata.Services.Extensions
             logger.LogError($"Formatting Error: {line}");
             throw new FormatException("Formatting Error");
         }
+
+        public static bool IsValidSpeed(this Trip trip)
+        {
+            return trip.IsAvgSpeedGreaterThan5Mph() && trip.IsAvgSpeedLessThan100Mph();
+        }
+
+        private static bool IsAvgSpeedGreaterThan5Mph(this Trip trip)
+        {
+            var mph = trip.MilesDriven / trip.TripTime;
+
+            if (mph > 5)
+                return true;
+            return false;
+        }
+
+        private static bool IsAvgSpeedLessThan100Mph(this Trip trip)
+        {
+            var mph = trip.MilesDriven / trip.TripTime;
+
+            if (mph < 100)
+                return true;
+            return false;
+        }
     }
 }
